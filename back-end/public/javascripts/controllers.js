49eef57c9514f777919533acd76464b4cdaa845f
Rodolfo Oviedo <rodolfo.oviedo@prodigious.cr>
2014-01-25 15:47:37 -0600

    Including implementation to load the selected candidate for voting

--- back-end/public/javascripts/controllers.js
+++ back-end/public/javascripts/controllers.js
@@ -3,13 +3,27 @@ var votacionescrControllers = angular.module('votacionescrControllers', []);
 votacionescrControllers.controller('VoterCtrl', ['$scope', 'PoliticalParty', function ($scope, PoliticalParty) {
   $scope.cantidates = [];
   $scope.politicalParties = [];
-
-
-
+  $scope.actualSelectedCandidate = {};
+  
+  /**
+   * Method to load the candidates from the database.
+   */
   var loadCandidates = function(){
   	$scope.politicalParties = PoliticalParty.query();
-  	console.log('load')
   }
 
+  /**
+   * Method to select a political party for vote
+   * @param partyId String with the value of the selected candidate
+   */
+  $scope.selectPartyForVote = function(partyId) {
+	  angular.forEach($scope.politicalParties, function(candidate, key) {
+		  // Check if the actual value is the partyId
+		  if (candidate._id === partyId) {
+			  $scope.actualSelectedCandidate = candidate;
+		  }
+	  });
+  }
+  
   loadCandidates();
 }]);
